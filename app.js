0d93e2dbf975698b1c339edf12362363f7ed5d5e
Loris <45939615+lchastanet@users.noreply.github.com>
2022-11-07 17:41:55 +0100

    remove semi

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const pictureSelector = document.querySelector("#picture-selector");
-const memeInput = document.querySelector("#meme-input");
-const memeText = document.querySelector("#meme-text");
-const printButton = document.querySelector("#print-button");
-const meme = document.querySelector("#meme");
+const pictureSelector = document.querySelector("#picture-selector")
+const memeInput = document.querySelector("#meme-input")
+const memeText = document.querySelector("#meme-text")
+const printButton = document.querySelector("#print-button")
+const meme = document.querySelector("#meme")
 
 pictureSelector.addEventListener("change", (e) => {
-  const img = window.URL.createObjectURL(e.target.files[0]);
-  meme.style.backgroundImage = `url('${img}')`;
-});
+  const img = window.URL.createObjectURL(e.target.files[0])
+  meme.style.backgroundImage = `url('${img}')`
+})
 
 memeInput.addEventListener("keyup", (e) => {
-  memeText.innerText = e.target.value;
-});
+  memeText.innerText = e.target.value
+})
 
-printButton.addEventListener("click", (e) => {
-  html2canvas(meme, { allowTaint: true }).then(function (canvas) {
-    const data = canvas.toDataURL("image/jpeg", 1);
-    const src = encodeURI(data);
-    const a = document.createElement("a");
-    a.setAttribute("download", "wild-meme.jpg");
-    a.setAttribute("href", src);
-    a.click();
-  });
-});
+printButton.addEventListener("click", () => {
+  html2canvas(meme, { allowTaint: true }).then((c) => {
+    const data = c.toDataURL("image/jpeg", 1)
+    const src = encodeURI(data)
+    const a = document.createElement("a")
+    a.setAttribute("download", "wild-meme.jpg")
+    a.setAttribute("href", src)
+    a.click()
+  })
+})
