e3b1912685cfc4a8ced79cb818795b0a86e81e54
Loris <45939615+lchastanet@users.noreply.github.com>
2022-11-07 17:35:37 +0100

    init files

---
+++ app.js
@@ -0,0 +1,26 @@
+const pictureSelector = document.querySelector("#picture-selector");
+const picture = document.querySelector("#picture");
+const memeInput = document.querySelector("#meme-input");
+const memeText = document.querySelector("#meme-text");
+const printButton = document.querySelector("#print-button");
+const meme = document.querySelector("#meme");
+
+pictureSelector.addEventListener("change", (e) => {
+  const img = window.URL.createObjectURL(e.target.files[0]);
+  meme.style.backgroundImage = `url('${img}')`;
+});
+
+memeInput.addEventListener("keyup", (e) => {
+  memeText.innerText = e.target.value;
+});
+
+printButton.addEventListener("click", (e) => {
+  html2canvas(meme, { allowTaint: true }).then(function (canvas) {
+    const data = canvas.toDataURL("image/jpeg", 1);
+    const src = encodeURI(data);
+    const a = document.createElement("a");
+    a.setAttribute("download", "wild-meme.jpg");
+    a.setAttribute("href", src);
+    a.click();
+  });
+});
